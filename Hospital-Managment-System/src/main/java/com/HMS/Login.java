8a79d188d9878819766730c6ba2f5f039ed1084f
Mayur londhe <Mayur londhe@LAPTOP-RPJIN03O>
2023-01-23 23:14:32 +0530

    Updated Code

--- Hospital-Managment-System/src/main/java/com/HMS/Login.java
+++ Hospital-Managment-System/src/main/java/com/HMS/Login.java
@@ -9,39 +9,34 @@ import jakarta.persistence.Id;
 @Entity
 public class Login {
 	@Id
-	String username;
-	String password;
-
+	private String username;
+	private String password;
 	public Login() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-
-	public Login(int id, String username, String password) {
+	public Login(String username, String password) {
 		super();
 		this.username = username;
 		this.password = password;
 	}
-
 	public String getUsername() {
 		return username;
 	}
-
 	public void setUsername(String username) {
 		this.username = username;
 	}
-
 	public String getPassword() {
 		return password;
 	}
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
 	@Override
 	public String toString() {
-		return "Login [ username=" + username + ", password=" + password + "]";
+		return "Login [username=" + username + ", password=" + password + "]";
 	}
+
+	
 
 }
